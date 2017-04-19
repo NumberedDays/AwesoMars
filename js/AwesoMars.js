56f70fd763dbeb6778b2abaec8a2d0f50cfbef06
NumberedDays <S02246660@FRCCinst.ccofc.edu>
2017-04-18 20:25:12 -0600

    Trying to get it to work
    Made some changes to link index.html to js & css in Brackets

--- js/AwesoMars.js
+++ js/AwesoMars.js
@@ -1,9 +1,15 @@
-L.mapbox.accessToken = 'pk.eyJ1IjoibnVtYmVyZWRkYXlzIiwiYSI6ImNpeTI0NmFuaDAwMTcycXBnZGZyaDJ1dGIifQ.fAD0O4m-jkeVUz_xZwNfpg';
-// Replace 'mapbox.streets' with your map id.
-var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.mars-satellite/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
-    attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-});
+document.addEventListener('DOMContentLoaded', function () {
 
-var map = L.map('map')
-    .addLayer(mapboxTiles)
-    .setView([42.3610, -71.0587], 8);
+            L.mapbox.accessToken = 'pk.eyJ1IjoibnVtYmVyZWRkYXlzIiwiYSI6ImNpeTI0NmFuaDAwMTcycXBnZGZyaDJ1dGIifQ.fAD0O4m-jkeVUz_xZwNfpg';
+
+            // Replace 'mapbox.streets' with your map id.
+
+            var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/mapbox.mars-satellite/{z}/{x}/{y}.png?access_token=' + L.mapbox.accessToken, {
+                attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            });
+
+            var map = L.map('map')
+                .addLayer(mapboxTiles)
+                .setView([42.3610, -71.0587], 8);
+
+        }
